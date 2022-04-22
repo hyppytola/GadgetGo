3315855582b85e9420ba480e37cd45c093226c04
hyppytola <domolakova.02@gmail.com>
2022-04-22 16:01:30 +0600

    delete directory

--- backend/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import {port} from 'config/config.json';
-
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(port);
-}
-bootstrap();
