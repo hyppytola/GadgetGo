beb86eb652ea21d5a46a66cf45f697f5a786dbd7
kshukaeva <kshukaevaaa@gmail.com>
2022-05-13 15:50:29 +0600

    Update Home Page

--- client/src/components/mainpages/homepage/HomePage.js
+++ client/src/components/mainpages/homepage/HomePage.js
@@ -21,31 +21,69 @@ function HomePage() {
                 New digital technologies, the most fashionable gadgets, innovative electronics products – in one place you can find everything that you need. In GadjetGo online store you will find modern smartphones, computers, printers, laptops, and much more
                 </p>
                 <p>
-                    Keep in mind that this isn't a fully functioning online storefront (yet): there's a bug in the shopping cart/bag, there are no sizing options, the contact form isn't fully functional, and there is no backend or complete checkout process. I'm sure there are other problems that I haven't unearthed. Despite this, I'm extremely satisfied with the results and what I've learned so far. I plan to revisit this project in the future to add a backend and make it fully functional.
+                E-commerce is the term used to describe the activity of buying and selling things through an online service or over the internet, and the people who engage in it are known as ecommers.
                 </p>
                 <p>
-                    All shop content (images & item names) was taken from <a href="https://madewell.com" target="new">Madewell</a>. Without their items, I would have had a pretty tough time curating a store.
+                Best electronics for best price!
                 </p>
                 <p>
-                    So, what went into this project?
-                    <ul>
-                        <li><a href="https://reactjs.org/" target="new">React</a></li>
-                        <li><a href="https://redux.js.org/" target="new">Redux</a></li>
-                        <li><a href="https://github.com/rt2zz/redux-persist" target="new">redux-persist</a></li>
-                        <li><a href="https://github.com/ReactTraining/react-router" target="new">react-router</a></li>
-                        <li><a href="https://github.com/adamrisberg/react-image-magnifiers" target="new">react-image-magnifiers</a></li>
-                        <li><a href="https://lodash.com/" target="new">Lodash</a></li>
-                        <li><a href="https://github.com/jaredpalmer/formik" target="new">Formik</a></li>
-                        <li><a href="https://github.com/jquense/yup" target="new">Yup</a></li>
-                        <li><a href="https://fontawesome.com/" target="new">Font Awesome</a></li>
-                        <li>And a whole lot of CSS</li>
-                    </ul>
-                </p>
-                <p>
-                    Thanks for stopping by.
+                On the Internet in the twenty-first century, there is such a wide range of information. You can do almost everything here, including attending classes, seminars, traveling, learning, and purchasing food, clothing, and other necessities for survival. When it comes to the online shopping system, we can state that it is well-established because the websites you visit may provide you a wide range of products to choose from. If they don't have anything on their website, they'll tell you to go somewhere else to get it. The plan is simple. E-commerce is the term used to describe the activity of buying and selling things through an online service or over the internet, and the people who engage in it are known as ecommers.
+
+GadgetGo online hardware store is a platform for online purchases of various electronic devices, where everyone has the opportunity to purchase devices, view reviews of the desired product. New digital technologies, the most fashionable gadgets, innovative electronics products – in one place you can find everything that you need. In GadjetGo online store you will find modern smartphones, computers, printers, laptops, and much more
+
+Problem&Solution
+Before starting a project, it is critical to do a feasibility assessment. People in the twenty-first century are now content to shop online. Electronic devices, in particular, are extremely popular on the Internet, and the client wanted to ensure that the products were appropriate for them. We need to make sure that when we build this e-commerce website, the visitor or buyer will be able to examine everything before purchasing the goods.
+
+The proposal of this project
+The major goal of our ecommerce website is to make it user-friendly and convenient to purchase things online in the same way that you would at a store.
                 </p>
             </div>
+            <div>
+            <section class="contact-wrapper" id="contact">
+        <div class="heading">
+            <h1>Contact us</h1>
+        </div>
+        <div class="contact-box">
+            <div class="left-contact">
+               <div class="bigg-circle"></div>
+
+               <div class="ssmall-circle"></div>
+                <div class="small-circle"></div>
+                <div class="big-circle"></div>
+
             </div>
+            <div class="right-contact">
+               <div class="form-container">
+                   <div class="form">
+                   <h1>Contact Information</h1>
+                       <p>Any Question and feedback? just write us a message</p>
+                <div class="phone  left-icon">
+                    <h3>+707 707 77 77</h3>
+                </div>
+                <div class="phone  left-icon">
+                    <h3>+777 777 77 77</h3>
+                </div>
+
+                <div class="mail left-icon">
+                    <h3>gadgetgo@gmail.com</h3>
+                </div>
+
+                <div class="location left-icon">
+                    <h3>Nur-Sultan, Kazakhstan</h3>
+                </div>
+                       
+
+                    </div>
+                   
+                </div>
+            </div>
+
+        </div>
+    </section>
+    
+            </div>
+            </div>
+            
     )
 }
 
